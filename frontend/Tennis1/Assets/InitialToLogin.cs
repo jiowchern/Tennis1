07fe1554f578aa9e5bec8b85c5d2a18a5bf23cd3
jc <jiowchern@gmail.com>
2019-12-19 02:41:37 +0800

    切換到登入畫面

---
+++ frontend/Tennis1/Assets/InitialToLogin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialToLogin : MonoBehaviour
+{
+      
+    // Update is called once per frame
+    public void ToLogin()
+    {
+        
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Login", UnityEngine.SceneManagement.LoadSceneMode.Additive).completed += InitialToLogin_completed;
+    }
+
+    private void InitialToLogin_completed(AsyncOperation obj)
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ frontend/Tennis1/Assets/Project/Scripts/Client.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using Regulus.Framework;
+using Tennis1.User;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    private  Client<User> _Client;
+
+    public Console Console;
+    readonly Regulus.Utility.Updater _Updater;
+
+
+    [System.Serializable]
+    public class InitialedUnityEvent : UnityEngine.Events.UnityEvent { };
+    public InitialedUnityEvent InitialedEvent;
+
+    public Client()
+    {
+        _Updater = new Regulus.Utility.Updater();
+    }
+    void Start()
+    {
+
+        _Client = new Regulus.Framework.Client<Tennis1.User.User>(this.Console, this.Console.Command);
+        _Client.ModeSelectorEvent += _Client_ModeSelectorEvent;
+        
+        _Updater.Add(_Client);
+
+
+    }
+
+    public void RunByRemote()
+    {
+        var asm = Regulus.Remote.Protocol.ProtocolProvider.GetProtocols().First().Assembly;
+        _Selector.AddFactoty("remote", new Tennis1.User.RemoteUserFactory(asm));
+        _Selector.CreateUserProvider("remote");
+    }
+
+    public void RunByStandalone()
+    {
+        var entry = new Tennis1.Game.Entry();
+        entry.Launch();
+        var asm = Regulus.Remote.Protocol.ProtocolProvider.GetProtocols().First().Assembly;
+        _Selector.AddFactoty("standalone", new Tennis1.User.StandaloneUserFactory(entry, asm));
+        _Selector.CreateUserProvider("standalone");
+
+    }
+
+    private void _Client_ModeSelectorEvent(GameModeSelector<User> selector)
+    {
+        _Selector = selector;
+        selector.GameConsoleEvent += Selector_GameConsoleEvent;
+        
+    }
+
+    private void Selector_GameConsoleEvent(UserProvider<User> provider)
+    {
+        User = provider.Spawn("User");
+        provider.Select("User");
+        _Updater.Add(User);
+        InitialedEvent.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Updater.Working();
+    }
+
+ 
+    private GameModeSelector<User> _Selector;
+
+    public User User { get; private set; }
+}
